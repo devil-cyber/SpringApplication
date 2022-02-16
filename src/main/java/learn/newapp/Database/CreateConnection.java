ba2962ea74fa1c3f7f894337dca4c32dad0c5219
devil-cyber <mani2474695@gmail.com>
2022-02-16 14:58:23 +0530

    Added new connection for sql

--- src/main/java/learn/newapp/Database/CreateConnection.java
+++ src/main/java/learn/newapp/Database/CreateConnection.java
@@ -3,14 +3,24 @@ package learn.newapp.Database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CreateConnection {
     public static Connection connect() {
+        String server = "mysql-69188-0.cloudclusters.net";
+        String dbName = "db";
+        String user = "admin";
+        String password = "WqbUgRZI";
+        int PORT = 19878;
+        String url = String.format("jdbc:mysql://%s:%d?user=%s&password=%s",
+                server, PORT, user, password);
         Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
-                    "newuser", "Mani360@kant");
+            connection = DriverManager.getConnection(url);
+            Statement stmt = connection.createStatement();
+            String sqlusedb = "use " + dbName;
+            int result = stmt.executeUpdate(sqlusedb);
         }catch (ClassNotFoundException | SQLException e){
             System.out.println(e);
         }
